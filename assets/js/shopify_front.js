d2e5b17c8fd2ac8d8e5890c54b5a410125c7cad2
CLS002 <julee.codelock99@gmail.com>
2023-07-27 18:46:01 +0530

    front popup set

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -59,7 +59,7 @@ include('https://codelocksolutions.com/easy-cookie-bar/assets/js/jquery-3.6.4.mi
                                     '<span class="bar-link"><a class="cc-link" href="'+ comeback.outcome.privacy_policy_url +'" target="_blank" style="text-decoration: underline;color:'+ comeback.outcome.color_banner_link  +'">'+ comeback.outcome.privacy_policy_url_text +'</a></span>'+
                                     '</span>'+
                                 '</div>'+
-                                ' <div class="three" style="width: 30%;">'+
+                                ' <div class="three">'+
                                     '<a class="cc-dismiss handleAccept" style="padding:7px 15px;color:'+ comeback.outcome.color_button_text +'; background-color:'+  comeback.outcome.color_button  +'; border-color: '+  comeback.outcome.color_button_border  +';border-radius:'+  comeback.outcome.button_border_radius  +';border:'+ comeback.outcome.border  +';">'+ comeback.outcome.agreement_text +'</a>'+
                                 '</div>'+
                                 '</div>'
