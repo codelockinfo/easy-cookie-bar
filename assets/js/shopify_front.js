756da280603770bc6ff9043fe6a052327e7c1fef
codelockinfo <codelockinfo@gmail.com>
2023-09-05 09:54:42 +0530

    shopify front file changess

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -33,11 +33,11 @@ include('https://codelocksolutions.com/easy-cookie-bar/assets/js/jquery-3.6.4.mi
               console.log("COMEBACK");
               console.log(comeback);
               console.log(comeback.outcome);
-                $status = comeback.outcome.application_status !== undefined ? comeback.outcome.application_status : '';
-                console.log(comeback.outcome.application_status +".....STATUS");
-                if ($status == 1) {
-                    cookiesbarSettingSelect();
-                }
+              $status = comeback.outcome.application_status !== undefined ? comeback.outcome.application_status : '';
+              console.log(comeback.outcome.application_status +".....STATUS");
+              if ($status == 1) {
+                  cookiesbarSettingSelect();
+              }
             }
         });    
         function  cookiesbarSettingSelect(){
